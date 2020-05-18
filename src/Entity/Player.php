71a01b461b1f33fd5682b92fb92e96d5b3e7a893
WallTakedown <4me3only@mail.ru>
2020-05-18 19:29:47 +0700

    Обновление (с оговорками)
    Пара непоняток на этом этапе:
    - как считается время (getPlayTime() не вызывается нигде, а дамп переменной player из шаблона не показывает никаких playTime);
    - как приватные свойства передаются в шаблон (у меня NULL приходит, свои делал публичными).

--- src/Entity/Player.php
+++ src/Entity/Player.php
@@ -11,6 +11,8 @@ class Player
     private string $playStatus;
     private int $inMinute;
     private int $outMinute;
+    public int $card;
+    public bool $scored;
 
     public function __construct(int $number, string $name)
     {
@@ -19,6 +21,8 @@ class Player
         $this->playStatus = self::BENCH_PLAY_STATUS;
         $this->inMinute = 0;
         $this->outMinute = 0;
+        $this->card = 0;
+        $this->scored = false;
     }
 
     public function getNumber(): int
@@ -53,6 +57,21 @@ class Player
         }
 
         return $this->outMinute - $this->inMinute;
+    }
+
+    public function issueCard(int $typeOfCard): void
+    {
+        $this->card += $typeOfCard;
+    }
+
+    public function getCard(): int
+    {
+        return $this->card;
+    }
+
+    public function scoreGoal(): void
+    {
+        $this->scored = true;
     }
 
     public function goToPlay(int $minute): void
